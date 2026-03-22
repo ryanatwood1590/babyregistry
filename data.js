72b70fb04afcc0f2903ee4b377b8b21d6545d6e3
ryanatwood1590 <ryan.atwood1590@gmail.com>
2026-03-22 11:55:31 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,59 @@
+// Baby Shower Site Data — generated 3/22/2026, 11:55:15 AM
+// Upload this file to your GitHub repo alongside index.html
+// It will automatically load for all visitors on all devices
+window.BS_SITE_DATA = {
+  "adminPw": "babyboy2026",
+  "envScript": "You've been invited...",
+  "envDate": "05 · 02 · 26",
+  "envReturn": "From: The Smith Family",
+  "taglinePrefix": "A Little",
+  "honoree": "Willow & Joe",
+  "tagline": "Silly Goose",
+  "subtagline": "Is About to Be on the Loose!",
+  "regLabel": "Registry",
+  "regUrl": "",
+  "babyName": "Baby Boy",
+  "rafflePrize": "a special prize at the shower",
+  "bunting": "Come Celebrate! 🦢",
+  "rsvpDeadline": "May 14th",
+  "rsvpNote": "We can't wait to celebrate with you!",
+  "polCap1": "memories",
+  "polCap2": "& love",
+  "month": "May",
+  "day": "28",
+  "time": "2:00 pm",
+  "venue": "The Goose Clubhouse",
+  "address": "123 Pond Ln · Gooseville, GA 1233",
+  "itinerary": [
+    {
+      "time": "2 PM",
+      "label": "Welcome & Snacks"
+    },
+    {
+      "time": "3 PM",
+      "label": "Games"
+    },
+    {
+      "time": "4 PM",
+      "label": "Gifts & Fun"
+    },
+    {
+      "time": "5 PM",
+      "label": "Departure"
+    }
+  ],
+  "sheetUrl": "https://script.google.com/macros/s/AKfycbwf2Cd585tzycJ1jz0YwWeS16DM95UWtcyKPL6_FWkz-JFF-wY0lUbZtssadxG1RfDfSA/exec",
+  "rsvpEnabled": true,
+  "guestCount": true,
+  "messageField": true,
+  "hiddenCards": [],
+  "customImages": [],
+  "emojiEnvStamp": "🦢",
+  "emojiEnvDeco": "🦢",
+  "emojiInvite": "🦢",
+  "emojiItinerary": "🦢",
+  "emojiRegistry": "🦢 🦢",
+  "emojiRaffle": "🦢 🎁 🦢",
+  "emojiRsvpBtn": "🦢",
+  "emojiRsvpNote": "🦢"
+};
